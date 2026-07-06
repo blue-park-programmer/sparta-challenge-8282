f0c2aefd2453e2c0b596efa782b2235f91453d70
dduddupark <develop.dduddu@gmail.com>
2026-07-06 14:37:29 +0900

    init commit

---
+++ src/test/java/com/sparta/spartachallenge8282/DeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartachallenge8282;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
